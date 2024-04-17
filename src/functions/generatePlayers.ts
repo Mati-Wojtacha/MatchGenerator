8c431eb83cb3ce180090a830447a05db771577af
Mati-Wojtacha <63257924+Mati-Wojtacha@users.noreply.github.com>
2024-04-17 22:42:58 +0200

    adding dist

--- src/functions/generatePlayers.ts
+++
@@ -1,8 +0,0 @@
-
-export function generatePlayers(teamId:number, name :string) {
-    const playerNames = [];
-    for (let i = 1; i <= teamId; i++) {
-        playerNames.push(name +` `+ i);
-    }
-    return playerNames;
-}
